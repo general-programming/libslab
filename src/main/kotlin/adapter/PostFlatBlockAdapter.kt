cef368808aa12b563c7bb0e14aa12b340535d08f
Henry <omgredstone@gmail.com>
2019-02-21 01:09:35 +0000

    Start of 1.13 block adapter

--- src/main/kotlin/adapter/PostFlatBlockAdapter.kt
+++ src/main/kotlin/adapter/PostFlatBlockAdapter.kt
@@ -8,10 +8,29 @@ import gq.genprog.libslab.block.Block
  */
 @VersionRange(GameVersion.JAVA_1_13)
 class PostFlatBlockAdapter: IBlockAdapter {
+    val fs: dynamic = js("require('fs')")
+    val mappings: Map<String, BlockDefinition> = JSON.parse(fs.readFileSync("resources/main/1.13.blocks.json", "UTF-8") as String)
 
     override fun Block.getId(): Int {
-        this.type.getFullResource()
+        val def = mappings[this.type.getFullResource()]
+
+        if (def != null)
+            return def.getDefaultState().id  // TODO: Handle metadata
 
         return 0
     }
+
+    data class BlockDefinition(val states: Array<BlockState>) {
+        fun getDefaultState(): BlockState {
+            for (state in states) {
+                if (state.default)
+                    return state
+            }
+
+            return states[0]
+        }
+    }
+
+
+    data class BlockState(val id: Int, val default: Boolean = false, val properties: Map<String, String>)
 }
--- src/main/kotlin/chunk/Chunk.kt
+++ src/main/kotlin/chunk/Chunk.kt
@@ -7,11 +7,11 @@ import gq.genprog.libslab.block.BlockType
  * Written by @offbeatwitch.
  * Licensed under MIT.
  */
-class Chunk {
+class Chunk(val height: Int = 256) {
     val blocks: Array<Array<Array<Block>>> =
         Array(16) { x ->
             Array(16) { z ->
-                Array(256) { y ->
+                Array(height) { y ->
                     Block(BlockType.AIR)
                 }
             }
